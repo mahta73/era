438084cc2e79f57d2d267fed6b7596064165c94c
MahtaMR <zohreh.torabian7366@gmail.com>
2017-07-06 14:20:51 +0400

    before adding firebase

--- src/frontend/mainPage.js
+++ src/frontend/mainPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, TouchableHighlight} from 'react-native';
 import { StackNavigator } from 'react-navigation';
 import styles from '../style';
 
@@ -7,11 +7,47 @@ import styles from '../style';
 export default class LoginPage  extends React.Component{
   constructor(props){
     super(props);
+    this.state = {
+      username:'',
+      Password:''
+    }
   }
   render(){
     return(
       <View>
-      <Text>Login page</Text>
+
+
+      <Image source = {require('../img/3855.png')} />
+
+
+      <Text>Era Registry</Text>
+
+
+      <TextInput
+     onChangeText={username => this.setState({username})}
+     placeholder = {"Username"}
+     placeHolderTextColor = 'black'
+     />
+
+
+      <TextInput
+      onChangeText={Password => this.setState({Password})}
+      placeholder = {"Password"}
+      secureTextEntry = {true}
+      placeHolderTextColor = 'black'
+      />
+
+      <TouchableHighlight
+        underlayColor = {'transparent'}
+       >
+     <View>
+     <Text>
+      Sign in
+      </Text>
+      </View>
+      </TouchableHighlight>
+
+
       </View>
     );
   }
